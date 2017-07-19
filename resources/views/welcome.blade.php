1a77258ff8bf824cedc62f0c19433262c1c0e5b6
Mate Molnar <matemolnar88@gmail.com>
2017-07-19 16:21:26 +0100

    Csináltam + s-t
    próba Gábornak

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Envienta
+                    Envienta(S)
                 </div>
 
                 <div class="links">
