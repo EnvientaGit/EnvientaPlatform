9e5442c4fde35a5653c329441d9d50834b885de1
Mate Molnar <matemolnar88@gmail.com>
2017-07-19 02:41:02 +0100

    Test
    Test: change the title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Envienta
                 </div>
 
                 <div class="links">
