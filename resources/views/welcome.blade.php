50cf73f0bcf6039cfb206a673dd22ca8f2ba2a5d
Mate Molnar <matemolnar88@gmail.com>
2017-07-25 15:03:30 +0100

    Capmaign
    The second site is updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,17 +78,18 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Envienta
+                <div class="title">
+                    <p>Envienta <br> Open Source Everything</p>
+                <img src="{{ URL::to('img/page-under-construction.png') }}" alt="">
                 </div>
 
-                <div class="links">
+                {{-- <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div> --}}
             </div>
         </div>
     </body>
