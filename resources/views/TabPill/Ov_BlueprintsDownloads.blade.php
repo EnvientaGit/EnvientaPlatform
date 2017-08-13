0bf06a19079bcb9b777ebe68cb9b1cf1f79eeb0d
Mate Molnar <matemolnar88@gmail.com>
2017-08-13 20:19:43 +0200

    first steps of the tabs at the overview

--- resources/views/TabPill/Ov_BlueprintsDownloads.blade.php
+++ resources/views/TabPill/Ov_BlueprintsDownloads.blade.php
@@ -1,3 +1,46 @@
 <h3 class="text-info">BLUEPRINTS</h3>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<hr>
 <iframe src="https://3dwarehouse.sketchup.com/embed.html?mid=f9c78f177ea35a4bf5926f6cee661177&width=580&height=326" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" width="694.60" height="435" allowfullscreen></iframe>
+<hr>
+<table class="table table-striped my-3">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+<br>
+<div class="list-group">
+  <a href="#" class="list-group-item active">
+    Cras justo odio
+  </a>
+  <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+  <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+  <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+  <a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+</div>
+{{-- hover the rowa! then you will get what you're looking for!! --}}
