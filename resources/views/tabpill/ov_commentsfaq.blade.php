f404966478cd4fa7a6152c60136dfc0f38a86fa6
Mate Molnar <matemolnar88@gmail.com>
2017-08-18 22:56:09 +0200

    comment_section

--- resources/views/tabpill/ov_commentsfaq.blade.php
+++ resources/views/tabpill/ov_commentsfaq.blade.php
@@ -6,6 +6,11 @@
             		<h3 class="card-header ftitle">Comments</h3>
           		</div>
         	</div>
+        	<div class="card-block">
+		    	<p class="card-text">
+		    		Aperiam? Perferendis, cursus pellentesque autem auctor, luctus ducimus, a, dui atque turpis? Quasi! Wisi maecenas dictum vitae urna! Illo quibusdam earum risus varius fames vitae, deleniti vero. Felis harum. Mi. Molestiae repellendus nostrud aliquet beatae bibendum. Scelerisque ut hic optio? Dictumst vivamus, augue culpa, quo autem, incidunt nulla, tenetur unde.
+		    	</p>
+		  	</div>
     	</div>
 	</div>
 </div>
