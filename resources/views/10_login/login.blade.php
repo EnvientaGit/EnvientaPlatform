1c1ab4a46375411f83c4f81b34d22c348ba56fe8
Thebojda <thebojda@gmail.com>
2018-02-08 10:32:58 +0100

    login

--- resources/views/10_login/login.blade.php
+++ resources/views/10_login/login.blade.php
@@ -6,24 +6,30 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
-        <p>Type your email address below to get a <i>pin code</i> to log in.</p>
-        <form id="login_form" method="post" action="{{ URL::to('/auth/login') }}">
+          
+        <div id="mail_block">
+          <p>Type your email address below to get a <i>pin code</i> to log in.</p>
           <div class="input-group">
             <input id="email" type="email" class="input-large" name="email" placeholder="Enter email address">
             <button id="login_form_submit" class="btn btn-info">Send pin code</button>
             <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
             <input type="hidden" name="captcha_token" id="login_form_token" />
           </div>
+        </div>
+
+        <div id="pin_block" style="display: none;">
+          <p>Type your <i>pin code</i> to log in.</p>
           <div class="input-group">
             <input type="text" name="pin" class="pincode6" maxlength="6">
             <button id="login_form_submit" class="btn btn-info">Log in</button>
             <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
             <input type="hidden" name="captcha_token" id="login_form_token" />
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-info" data-dismiss="modal">Cancel</button>
-          </div>
-        </form>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-info" data-dismiss="modal">Cancel</button>
+        </div>
       </div>
       
     </div><!-- /.modal-content -->
