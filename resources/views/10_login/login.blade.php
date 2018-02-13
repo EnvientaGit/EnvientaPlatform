fa3912462b7d6e3238d2cf79b26e3ba1c7a2b368
Mate Molnar <matemolnar88@gmail.com>
2018-02-13 19:10:46 +0100

    login - facelift #2

--- resources/views/10_login/login.blade.php
+++ resources/views/10_login/login.blade.php
@@ -7,13 +7,14 @@
       </h6>
 
       <div class="card-body">
-   
+        
+        {{-- Enter mail part --}}
         <div id="mail_block">
           <div id="email_alert" class="alert alert-danger" style="display: none;">
-            Invalid <strong>pin code</strong>! Please try again or stop hacking.
+            Invalid <strong>pin code</strong>! Please try again and/or stop hacking!
           </div>
 
-          <p class="card-text text-justify">Type your email address below to get a <i>pin code</i> to log in.</p>
+          <p class="card-text text-justify">Type your email address below to get a <b class="text-success">pin code</b> to log in.</p>
 
           <div class="input-group mb-3">
             <div class="input-group-prepend">
@@ -21,19 +22,27 @@
             </div>
             <input type="email" id="request_pin_email" name="email" class="form-control btn-sm" placeholder="Enter email address" aria-label="Username" aria-describedby="basic-addon1">
           </div>
-          
+
           <div class="input-group mb-3">
-            <button id="request_pin_submit" class="btn btn-sm btn-info">Send pin code</button>
+            <button id="request_pin_submit" class="btn btn-sm btn-info env_right">Send pin code</button>
             <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
             <input type="hidden" name="captcha_token" id="login_form_token" />
           </div>
         </div>
-
+        
+        {{-- Enter Pin part --}}
         <div id="pin_block" style="display: none;">
-          <p class="card-text text-justify">Type your <i>pin code</i> to log in.</p>
-          <div class="input-group">
-            <input id="login_pin" type="text" name="pin" maxlength="6">
-            <button id="login_submit" class="btn btn-info">Log in</button>
+          <p class="card-text text-justify">Type your <b class="text-success">pin code</b> to log in.</p>
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1"><i class="fa fa-key" aria-hidden="true"></i></span>
+            </div>
+            <input id="login_pin" type="text" name="pin" maxlength="6" placeholder="Enter pin code" class="form-control btn-sm">
+          </div>
+          
+          <div class="input-group mb-3">
+            <button id="login_submit" class="btn btn-sm btn-info env_right">Log in</button>
             <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
             <input type="hidden" name="captcha_token" id="login_form_token" />
           </div>
