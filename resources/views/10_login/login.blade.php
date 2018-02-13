a169a572cbf63a790a214b027a327f42e91d8339
Mate Molnar <matemolnar88@gmail.com>
2018-02-13 17:59:31 +0100

    login - facelift #1

--- resources/views/10_login/login.blade.php
+++ resources/views/10_login/login.blade.php
@@ -1,27 +1,36 @@
 <div id="login_dialog" class="modal animated bounceIn" tabindex="-1" role="dialog" aria-labelledby="modalLabelLarge" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title">Log In - Simply with an email!</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      </div>
-      <div class="modal-body">
-          
+      
+      <h6 class="card-header dtitle p-2">Log In - Simply with an email!
+        <i class="fa fa-times env_edit pull-right text-secondary" aria-hidden="true" data-dismiss="modal" aria-label="Close"></i>
+      </h6>
+
+      <div class="card-body">
+   
         <div id="mail_block">
           <div id="email_alert" class="alert alert-danger" style="display: none;">
             Invalid <strong>pin code</strong>! Please try again or stop hacking.
           </div>
-          <p>Type your email address below to get a <i>pin code</i> to log in.</p>
-          <div class="input-group">
-            <input id="request_pin_email" type="email" class="input-large" name="email" placeholder="Enter email address">
-            <button id="request_pin_submit" class="btn btn-info">Send pin code</button>
+
+          <p class="card-text text-justify">Type your email address below to get a <i>pin code</i> to log in.</p>
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+            </div>
+            <input type="email" id="request_pin_email" name="email" class="form-control btn-sm" placeholder="Enter email address" aria-label="Username" aria-describedby="basic-addon1">
+          </div>
+          
+          <div class="input-group mb-3">
+            <button id="request_pin_submit" class="btn btn-sm btn-info">Send pin code</button>
             <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
             <input type="hidden" name="captcha_token" id="login_form_token" />
           </div>
         </div>
 
         <div id="pin_block" style="display: none;">
-          <p>Type your <i>pin code</i> to log in.</p>
+          <p class="card-text text-justify">Type your <i>pin code</i> to log in.</p>
           <div class="input-group">
             <input id="login_pin" type="text" name="pin" maxlength="6">
             <button id="login_submit" class="btn btn-info">Log in</button>
@@ -30,11 +39,10 @@
           </div>
         </div>
 
-        <br/>
+      </div>
 
-        <div class="modal-footer">
-          <button type="button" class="btn btn-outline-info" data-dismiss="modal">Cancel</button>
-        </div>
+      <div class="card-footer p-2">
+          <button type="button" class="btn btn-sm btn-outline-info pull-right" data-dismiss="modal">Cancel</button>
       </div>
       
     </div><!-- /.modal-content -->
