c1881d2292709b0a675b88e274c2114ebed4228e
Thebojda <thebojda@gmail.com>
2018-02-18 12:42:08 +0100

    laci stye hack

--- resources/views/20_platform/campaign.blade.php
+++ resources/views/20_platform/campaign.blade.php
@@ -4,16 +4,16 @@
 
     <main role="main">
 
-      <section id="env_board" class="jumbotron text-center">
+      <section id="env_board" class="jumbotron text-center" style="background-image: url('http://hackthewatercrisis.org/assets/images/cape-town-2000x1333.jpg'); background-size:cover; background-position: 0px -150px;">
         <div class="container">
           {{--<h1 class="jumbotron-heading">Projects for Cape Town</h1>--}}
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/wokSwsu3-0o?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-          <p class="lead text-muted">
+          {{--<iframe width="560" height="315" src="https://www.youtube.com/embed/wokSwsu3-0o?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>--}}
+          <p class="lead" style="color: #efefef; font-size: 20px; font-weight: 400;">
             “We, at the ENVIENTA™ believe that technological progress can bring abundance and unlimited wealth to the people.
             But this is only possible if every human beings are able to access to the technology.”
           </p>
           <p>
-            <button type="button" class="btn btn-primary env_button" data-toggle="modal" data-target="#newProjectModal">Create new project</button>
+            <button type="button" class="btn btn-lg" data-toggle="modal" data-target="#newProjectModal">Create new project</button>
             {{-- <a href="#" class="btn btn-secondary my-2">Button_2</a> --}}
           </p>
         </div>
