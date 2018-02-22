37580f9d63209fb959729bf7b603b3c82c7abfae
Thebojda <thebojda@gmail.com>
2018-02-22 12:08:06 +0100

    btn

--- resources/views/40_topbar/navigation.blade.php
+++ resources/views/40_topbar/navigation.blade.php
@@ -7,7 +7,7 @@
             <p class="env_left">
               <span class="badge badge-danger font-italic">Beta!</span>
             </p>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #f8f8f8;">
               <span class="navbar-toggler-icon"></span>
             </button>
 
