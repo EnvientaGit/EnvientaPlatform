6d8d8855a563058b2c98f0b5c050e3b341607142
Mate Molnar <matemolnar88@gmail.com>
2018-04-29 15:42:46 +0200

    Update - ReStore: - Disqus function switched back - Describe tabs order switched back

--- resources/views/50_project/51_tabs/comments.blade.php
+++ resources/views/50_project/51_tabs/comments.blade.php
@@ -2,17 +2,17 @@
 	<div class="container">
 		<div id="disqus_thread"></div>
 		<script>
-			// (function() { // DON'T EDIT BELOW THIS LINE
+			(function() { // DON'T EDIT BELOW THIS LINE
 
-			// var d = document, s = d.createElement('script');
-			// 	s.src = 'https://http-platform-envienta-org.disqus.com/embed.js';
-			// 	s.setAttribute('data-timestamp', +new Date());
-			// 		(d.head || d.body).appendChild(s);
-			// 	})();
+			var d = document, s = d.createElement('script');
+				s.src = 'https://http-platform-envienta-org.disqus.com/embed.js';
+				s.setAttribute('data-timestamp', +new Date());
+					(d.head || d.body).appendChild(s);
+				})();
 				
 		</script>
 		<noscript>Please enable JavaScript to view the 
-			{{-- <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a> --}}
+			<a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
 		</noscript>
 	</div>
 </div>
--- resources/views/50_project/description.blade.php
+++ resources/views/50_project/description.blade.php
@@ -2,13 +2,13 @@
   <div class="col-md-12">
     <ul class="nav nav-pills text-center">
       <li class="nav-item col-md-3">
-        <a data-toggle="pill" href="#project_details" class="btn btn-sm btn-light bg-light nav-link box-shadow-bottom">Details</a>
+        <a data-toggle="pill" href="#project_details" class="btn btn-sm btn-light bg-light nav-link active box-shadow-bottom">Details</a>
       </li>
       <li class="nav-item col-md-3">
         <a data-toggle="pill" href="#project_files" class="btn btn-sm btn-light bg-light nav-link box-shadow-bottom">Files</a>
       </li>
       <li class="nav-item col-md-3">
-        <a data-toggle="pill" href="#menu3" class="btn btn-sm btn-light bg-light nav-link active box-shadow-bottom">News Feed</a>
+        <a data-toggle="pill" href="#menu3" class="btn btn-sm btn-light bg-light nav-link box-shadow-bottom">News Feed</a>
       </li>
       <li class="nav-item col-md-3">
         <a data-toggle="pill" href="#menu2" class="btn btn-sm btn-light bg-light nav-link box-shadow-bottom">Comments</a>        
@@ -17,13 +17,13 @@
   </div>
 </div>
 <div class="tab-content mt-3">
-  <div id="project_details" class="tab-pane animated fadeIn">
+  <div id="project_details" class="tab-pane animated active fadeIn">
       @include('50_project.51_tabs.details')
   </div>
   <div id="project_files" class="tab-pane animated fadeIn">
       @include('50_project.51_tabs.blueprints')
   </div>
-  <div id="menu3" class="tab-pane animated fadeIn active">
+  <div id="menu3" class="tab-pane animated fadeIn">
       @include('50_project.51_tabs.post')
   </div>
   <div id="menu2" class="tab-pane animated fadeIn">
