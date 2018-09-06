a2eb99013fb67445e5fd204becf3a85eaa87b697
Mate <matemolnar88@gmail.com>
2018-09-07 00:59:37 +0200

    order mail updated

--- resources/views/mail/order.blade.php
+++ resources/views/mail/order.blade.php
@@ -1,12 +1,22 @@
-<html>
-	<body>
-		Name: {{$user->realname}}<br/>
-		Address: {{$user->customerAddress}}<br/>
-		Phone: {{$user->customerPhone}}<br/>
-		Billing address: {{$user->customerBillingAddress}}<br/>
-		E-mail: {{$user->email}}<br/><br/>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Makerspace order</title>
+</head>
+<body>
+  <style>
+    
+  </style>
 
-		Product name: {{$productName}}<br/>
-		Product url: {{$productUrl}}<br/>
-	</body>
+  Name: {{$user->realname}}<br/>
+  Address: {{$user->customerAddress}}<br/>
+  Phone: {{$user->customerPhone}}<br/>
+  Billing address: {{$user->customerBillingAddress}}<br/><br/>
+
+  Product name: {{$productName}}<br/>
+  Product url: {{$productUrl}}<br/>
+</body>
 </html>
