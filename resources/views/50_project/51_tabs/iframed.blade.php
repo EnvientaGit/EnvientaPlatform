a50a410a5a3c66c24826c0470bf4934bf59d971e
matemolnar88 <matemolnar88@gmail.com>
2018-11-12 13:52:31 +0100

    iframe - frame fix

--- resources/views/50_project/51_tabs/iframed.blade.php
+++ resources/views/50_project/51_tabs/iframed.blade.php
@@ -1,3 +1,3 @@
-<iframe class="env_mirror w-100" src="http://newsfeed.envienta.org">
+<iframe class="env_mirror w-100" src="http://newsfeed.envienta.org" frameborder="0" scrolling="no">
   <p>Your browser does not support iframes.</p>
 </iframe>
