67d8eb9f53d86d88080b1902c6969de6d4bbd147
matemolnar88 <matemolnar88@gmail.com>
2018-12-04 15:41:42 +0100

    Updated fab

--- resources/views/00_header/platform.blade.php
+++ resources/views/00_header/platform.blade.php
@@ -108,7 +108,7 @@
             @include('90_footer.footer')
 
             <!-- Floating Action Button - FAB - Telegram  -->
-            <div class="">
+            <div id="fab">
               <!-- <i class="fab fa-telegram fa-3x telegram_button" data-toggle="popover" data-placement="top" title="Need help?" data-content="Click here to reach our Telegram channel!"></i> -->
               <img class="telegram_button" src="{{ URL::to('img/telegram_logo.svg') }}" data-toggle="popover" data-placement="left" title="Need help?" data-content="<a href='https://t.me/envienta' title='telegram link'><button class='btn btn-primary btn-sm'>Join </button></a> our Telegram channel!"  width="64" height="64" alt="Telegram">
             </div>
@@ -125,6 +125,18 @@
             })
           })
           $("[data-toggle=popover]").popover({html:true})
+
+          $(window).scroll(function() {
+            var heightTop = $('#fab').offset().top,
+            outHeight = $('#fab').outerHeight(),
+            wHeight = $(window).height(),
+            wScroll = $(this).scrollTop();
+            if (wScroll > (heightTop+outHeight-wHeight) && (heightTop > wScroll) && (wScroll+wHeight > heightTop+outHeight)){
+              $('.telegram_button').fadeOut("slow");
+            } else {
+              $('.telegram_button').fadeIn("slow");
+            }
+          });
         </script>
     </body>
 </html>
