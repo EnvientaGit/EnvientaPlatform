26cf0b6b1a7de5ac4ac6713bca4fbb126b1fbcd8
matemolnar88 <matemolnar88@gmail.com>
2018-12-04 13:19:27 +0100

    Fix(?)

--- resources/views/00_header/platform.blade.php
+++ resources/views/00_header/platform.blade.php
@@ -107,15 +107,16 @@
 
             @include('90_footer.footer')
 
+            <!-- Floating Action Button - FAB - Telegram  -->
+            <div class="">
+              <!-- <i class="fab fa-telegram fa-3x telegram_button" data-toggle="popover" data-placement="top" title="Need help?" data-content="Click here to reach our Telegram channel!"></i> -->
+              <img class="telegram_button" src="{{ URL::to('img/telegram_logo.svg') }}" data-toggle="popover" data-placement="left" title="Need help?" data-content="<a href='https://t.me/envienta' title='telegram link'><button class='btn btn-primary btn-sm'>Join </button></a> our Telegram channel!"  width="64" height="64" alt="Telegram">
+            </div>
+
             <!-- extrajs -->
             <?php echo $EXTRAJS; ?>
         </div>
 
-        <!-- Floating Action Button - FAB - Telegram  -->
-        <div class="">
-          <!-- <i class="fab fa-telegram fa-3x telegram_button" data-toggle="popover" data-placement="top" title="Need help?" data-content="Click here to reach our Telegram channel!"></i> -->
-          <img class="telegram_button" src="{{ URL::to('img/telegram_logo.svg') }}" data-toggle="popover" data-placement="left" title="Need help?" data-content="<a href='https://t.me/envienta' title='telegram link'><button class='btn btn-primary btn-sm'>Join </button></a> our Telegram channel!"  width="64" height="64" alt="Telegram">
-        </div>
 
         <script type="text/javascript">
           $(function () {
