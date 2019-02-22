01b5d2466209dec9f874110c58113b3c0c9759d5
utopszkij <tibor.fogler@gmail.com>
2019-02-22 11:58:45 +0100

    multylanguage frontend inworking...

---
+++ resources/lang/en/front.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | front page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'STARTUP' => 'STARTUP ACCELERATOR FOR CLOUD-BASED MANUFACTURING',
+    'INFO' => 'first, blockchain based system aims to speed up the collaborative open source hardware development, from ideas to prototypes, through manufacturing, sales and distribution, based on local circular economy principles.',
+    'CREATE_PROJECT' => 'CREATE NEW PROJECT',
+    'CHOOSE_PROFILE' => 'CHOOSE YOUR PROFILE',
+    'INFO2' => 'ENVIENTA connects people and initiatives around the globe. They are all using the platform in the process of value creation and exchange as prosumers – it means the roles can change at any time, continuously.',
+    'FEEDBACK' => 'COMMUNITY FEEDBACK LOOPS',
+    'INFO3' => 'The platform is using data-based tools to create community feedback loops, gathering signals about the quality of blueprints, prototypes, products or - as a key factor in curation - the reputation of any service providers shaped by past behavior.<br><br>Real time social feedbacks and assessment of needs reduce the costs of market research, help to avoid overproduction - ultimately balance demand and supply.',
+    'HAVE_AN_IDEA' => 'Have an idea?',
+    'IDEA_INFO' => 'Start collaborating with others and collecting followers together right away, while showing your project details and updates.&nbsp;',
+    'START_CAMPAIGN' => 'Start a campaign',
+    'CAMPAIGN_INFO' => 'Need investments for designing and prototyping? Deal with market research and the acquisition of funds in one step.',
+    'DOWNLOAD' => 'Download for free',
+    'DOWNLOAD_INFO' => 'Once a prototype has been made, you are able to assemble the platform’s any approved product prototype on your own and use it.',
+    'DELIVERED' => 'From prototypes to delivered products',
+    'DELIVERED_INFO' => 'Let manufacturers and distributors find you and help to make your product wide accessible anywhere in the world .',
+    'MAKERS' => '',
+    'PROJECTSS' => '',
+    'INVESTORS' => '',
+    'MANUFACTURES' => '',
+    'DISTRIBUTORS' => '',
+    'CONSUMERS' => '',
+    'ALT_TECHNOLOGICAL' => 'ENVISION &amp; ENCOURAGE TECHNOLOGICAL ALTERNATIVES',
+    'ALT_TECHNOLOGICAL_INFO' => 'From establishing new designer collaborations and crowdfunding your open source projects to get into the supply chain, ENVIENTA has the tools.',
+    
+];
---
+++ resources/lang/hu/front.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | front page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'STARTUP' => 'STARTUP PROJEKTEK A FELHŐBEN',
+    'INFO' => 'blockchain alapú rendszer aminek a célja, hogy felgyorsítsa az együttműködő nyílt forráskódú hardverfejlesztést az ötletektől a prototípus gyártásig, az értékesítést és a forgalmazást, a helyi körkörös gazdaság elvei alapján.',
+    'CREATE_PROJECT' => 'ÚJ PROJEKT LÉTREHOZÁSA',
+    'CHOOSE_PROFILE' => 'Választható / változó szerepek',
+    'INFO2' => 'Az ENVIENTA világszerte összeköti az embereket és a kezdeményezéseket. Mindannyian ugyanazt a platformot használják a tervezéstől a termék felhasználásáig - ez azt jelenti, hogy az <br /> tervező / befektető / gyártó / forgalmazó / felhasználó szerepek bármikor, folyamatosan változhatnak / változnak.',
+    'FEEDBACK' => 'KÖZÖSSÉGI MINŐSÉG BIZTOSÍTÁS ÉS ERŐFORRÁS ELOSZTÁS',
+    'INFO3' => 'A platform WEB alapú eszközöket használ a közösségi visszajelzések kezelésére, információk gyűjtésére a tervrajzok, prototípusok, termékek minőségéről, vagy - a megbizhatóság kulcsfontosságú tényezőjéről - a korábbi tevékenység közösségi értékeléséről. A valós idejű társadalmi visszacsatolások és az igények felmérése csökkenti a piackutatás költségeit, segít a túltermelés elkerülésében - végső soron létrehozhatja a kereslet és a kínálat egyensúlyát.',
+    'HAVE_AN_IDEA' => 'Van egy ötleted?',
+    'IDEA_INFO' => 'Müködj együtt másokkal, gyorsan szerezz segítséget, támogatókat! Miközben közzé teszed a projekt részleteit és változásait.&nbsp;',
+    'START_CAMPAIGN' => 'Kampány indítása',
+    'CAMPAIGN_INFO' => 'Befektetésekre van szükség a tervezéshez és a prototípusok készítéséhez? Egy lépésben kezelheted a piackutatást és az erőforrások megszerzését.',
+    'DOWNLOAD' => 'Ingyenes letöltés',
+    'DOWNLOAD_INFO' => 'Miután elkészült a prototípus, a platform bármely jóváhagyott termékének prototípusát szabadon fejlszthetod, felhasználhatod.',
+    'DELIVERED' => 'A prototípusoktól a felhasználásig',
+    'DELIVERED_INFO' => 'Tedd lehetővé, hogy a gyártók és a forgalmazók megtaláljanak, és segíthessék a termék széleskörű gyártását felhasználását bárhol a világon.',
+    'MAKERS' => 'Tervezők',
+    'PROJECTSS' => 'Projektek',
+    'INVESTORS' => 'Befektetők',
+    'MANUFACTURES' => 'Gyártók',
+    'DISTRIBUTORS' => 'Terjesztők',
+    'CONSUMERS' => 'Felhasználók',
+    'ALT_TECHNOLOGICAL' => 'Új technológiai alternativák elterjesztése',
+    'ALT_TECHNOLOGICAL_INFO' => 'Az ENVIENA támogatja az új tervezési technológiák elterjedését',
+    
+];
