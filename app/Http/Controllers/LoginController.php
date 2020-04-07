b9f7b471326075b0ffbcf79b6e383dedb0c8cd6a
TheBojda <thebojda@gmail.com>
2020-04-08 01:06:21 +0200

    Firebase Auth for signin

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -15,18 +15,19 @@ use Illuminate\Database\Schema\Blueprint;
 class LoginController extends Controller
 {
 	/**
-	* if user not found create new user
-	* create new pin code, send email
-	* @params {email: 'xxxx'}
-	* @return 'sent'
-	*/
-	protected function newPinMail($request) {
-     	$user = User::updateOrCreate(['email' => $request->input('email')], ['pin' => Utils::random_str(6)]);
-  	  	Mail::to($user->email)->send(new LoginMail($user));
-     	return "sent";
-	}	
+	 * if user not found create new user
+	 * create new pin code, send email
+	 * @params {email: 'xxxx'}
+	 * @return 'sent'
+	 */
+	protected function newPinMail($request)
+	{
+		$user = User::updateOrCreate(['email' => $request->input('email')], ['pin' => Utils::random_str(6)]);
+		Mail::to($user->email)->send(new LoginMail($user));
+		return "sent";
+	}
 
-	 /**
+	/**
 	 * if user not found create new user
 	 * if mode=='pin' or user->psw == '') then
 	 *   create new pin and send email return "sent"
@@ -35,9 +36,10 @@ class LoginController extends Controller
 	 * @params {email:'xxx', mode:'?|pin' }
 	 * @return string 'sent'|'psw'
 	 */
-    public function requestPin(Request $request) {
+	public function requestPin(Request $request)
+	{
 		$result = "";
-     	$user = User::where('email', $request->input('email'))->first();     	
+		$user = User::where('email', $request->input('email'))->first();
 		// processing
 		if ($request->mode == 'pin') {
 			$result = $this->newPinMail($request);
@@ -47,63 +49,86 @@ class LoginController extends Controller
 					$result = "psw";
 				} else {
 					$result = $this->newPinMail($request);
-				}	
+				}
 			} else {
 				$result = $this->newPinMail($request);
-	    	}	
-     	}
-     	return $result;
-    }
+			}
+		}
+		return $result;
+	}
 
-	 /**
+	/**
 	 * if $request->psw is set then login by password
 	 * else login by pin
 	 */
-    public function login(Request $request) {
-    	if (isset($request->psw)) {
-    	    // $user = User::where('email', $request->input('email'))->where('psw', md5($request->input('psw')))->first();
-    	    $users = User::where('email', $request->input('email'));
-    	    $user = $users->first();
-    	    $user = $users->where('psw', md5($request->input('psw').$user->id))->first();
-    	    if($user) {
-	           Auth::login($user, true);
-	           return "success";
-	       } 
-    	} else {
-	      $user = User::where('email', $request->input('email'))->where('pin', $request->input('pin'))->first();
-	      if($user) {
-	        Auth::login($user, true);
-	        return "success";
-	      } 
-   		}
-      
-      	// always update to prevent brute force attacs
-      	User::updateOrCreate(['email' => $request->input('email')], ['pin' => Utils::random_str(6)]); 
-      	return "fail";
-    }
-    
-    public function logout() {
-      Auth::logout();
-      return redirect(url('/'));
-    }
+	public function login(Request $request)
+	{
+		if (isset($request->psw)) {
+			// $user = User::where('email', $request->input('email'))->where('psw', md5($request->input('psw')))->first();
+			$users = User::where('email', $request->input('email'));
+			$user = $users->first();
+			$user = $users->where('psw', md5($request->input('psw') . $user->id))->first();
+			if ($user) {
+				Auth::login($user, true);
+				return "success";
+			}
+		} else {
+			$user = User::where('email', $request->input('email'))->where('pin', $request->input('pin'))->first();
+			if ($user) {
+				Auth::login($user, true);
+				return "success";
+			}
+		}
 
-    public function loginWithFacebook(Request $request)
-    {
-      $token = $request->input('token');
-      $fb_user = Socialite::driver('facebook')->userFromToken($token);
-      if($fb_user) {
-        $user = User::updateOrCreate(
-          ['email' => $fb_user->getEmail()], 
-          [ 
-            'pin' => Utils::random_str(6), 
-            'avatarUrl' => $fb_user->getAvatar(), 
-            'profileUrl' => 'https://www.facebook.com/' . $fb_user->getId()
-          ]
-        );
-        Auth::login($user, true);
-        return "success";
-      } 
-      return "fail";  
-    }
+		// always update to prevent brute force attacs
+		User::updateOrCreate(['email' => $request->input('email')], ['pin' => Utils::random_str(6)]);
+		return "fail";
+	}
 
+	public function logout()
+	{
+		Auth::logout();
+		return redirect(url('/'));
+	}
+
+	public function loginWithFacebook(Request $request)
+	{
+		$token = $request->input('token');
+		$fb_user = Socialite::driver('facebook')->userFromToken($token);
+		if ($fb_user) {
+			$user = User::updateOrCreate(
+				['email' => $fb_user->getEmail()],
+				[
+					'pin' => Utils::random_str(6),
+					'avatarUrl' => $fb_user->getAvatar(),
+					'profileUrl' => 'https://www.facebook.com/' . $fb_user->getId()
+				]
+			);
+			Auth::login($user, true);
+			return "success";
+		}
+		return "fail";
+	}
+
+	public function loginWithFirebase(Request $request)
+	{
+		$token = $request->input('token');
+		$fbuser = Utils::getFirebaseUserByToken($token);
+		if ($fbuser) {
+			if (property_exists($fbuser, 'photoUrl'))
+				$photoUrl = $fbuser->photoUrl;
+			else
+				$photoUrl = "https://www.gravatar.com/avatar/" . md5(strtolower(trim($fbuser->email)));
+			$user = User::updateOrCreate(
+				['email' => $fbuser->email],
+				[
+					'pin' => Utils::random_str(6),
+					'avatarUrl' => $photoUrl
+				]
+			);
+			Auth::login($user, true);
+			return "success";
+		}
+		return "fail";
+	}
 }
